3b64747e3bd654601007dac4e503b284b89aa423
Grant Cohoe <grant@grantcohoe.com>
2019-06-28 14:43:50 -0400

    lazy

--- roles/shell/files/zsh/virtualenv.zsh
+++ roles/shell/files/zsh/virtualenv.zsh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-if [ -f /usr/local/bin/virtualenvwrapper.sh ]; then
-    . /usr/local/bin/virtualenvwrapper.sh
+if [ -f /usr/local/bin/virtualenvwrapper_lazy.sh ]; then
+    . /usr/local/bin/virtualenvwrapper_lazy.sh
 fi
